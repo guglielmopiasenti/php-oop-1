8f1f1afa1e05d8da1a21650786d65fb5a43bbd17
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-21 16:32:49 +0200

    methods + movies

--- index.php
+++ index.php
@@ -10,6 +10,13 @@ class Movie
         $this->title = $title;
         $this->release_date = $release_date;
     }
+    // methods
+    public function presentFilm()
+    {
+        return 'Movie title:' . $this->title . '' . 'Release Date:' . $this->release_date;
+    }
 }
 
+$movie1 = new Movie('Il Signore degli Anelli', '2001' );
+$movie2 = new Movie('The Truman Show', '1998');
 ?>
