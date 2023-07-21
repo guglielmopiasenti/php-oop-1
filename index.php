6cc2e29b44521bd0f50dd52147d8205e544c49ba
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-21 16:05:35 +0200

    class movie with instance and construct

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
+class Movie
+{
+    // instance
+    public $title;
+    public $release_date;
+    // construct
+    public function __construct(string $title, string $release_date)
+    {
+        $this->title = $title;
+        $this->release_date = $release_date;
+    }
+}
 
 ?>
