da13e2dbe35761a7ef1c2935482eca18df8a5c8b
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-25 17:26:25 +0200

    adding person.php

---
+++ oop_movies/Person.php
@@ -0,0 +1,18 @@
+<?php
+class Actor
+{
+    public $id;
+    public $first_name;
+    public $last_name;
+    public function __construct($id, $first_name, $last_name)
+    {
+        $this->id = $id;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+    }
+    public function getFullName()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+}
