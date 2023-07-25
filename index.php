9320bfd5c6705a4cef41dbda137d414142d23946
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-25 17:52:21 +0200

    restructuring index

--- index.php
+++ index.php
@@ -1,25 +1,34 @@
 <?php
-class Movie
-{
-    // instance
-    public $title;
-    public $release_date;
-    // construct
-    public function __construct(string $title, string $release_date)
-    {
-        $this->title = $title;
-        $this->release_date = $release_date;
-    }
-    // methods
-    public function presentFilm()
-    {
-        return 'Movie title: ' . $this->title . '<br>' . 'Release Date: ' . $this->release_date . '<br>';
-    }
-}
+include __DIR__ . '/oop_movies/Models/Person.php';
+include __DIR__ . '/oop_movies/Models/Movie.php';
 
-$movie1 = new Movie('Il Signore degli Anelli', '2001' );
-$movie2 = new Movie('The Truman Show', '1998');
 
-echo $movie1->presentFilm();
-echo $movie2->presentFilm();
+$movie1 = new Movie(1, 'Il Signore degli Anelli', 'Fantasy', '2001' );
+$movie2 = new Movie(2, 'The Truman Show', 'Comedy', '1998');
+
+$movies = [$movie1, $movie2];
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <h2>Movies</h2>
+        <div class="card">
+            <?php foreach($movies as $movie) :?>
+                <li>
+                    <h2><?=$movie->$title?></h2>
+                    <small><em><?=$movie->$genre?></em></small>
+                    <small><em><?=$movie->$release_date?></em></small>
+                </li>
+            <? endforeach;?>
+        </div>
+    </section>
+</body>
+</html>
